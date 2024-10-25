d56b0e83b5542ff7a85f948f94cedc23144c8a5a
satreya27 <shreyasatreya27@gmail.com>
2024-10-25 19:46:25 -0400

    Updated to Relevant Commands, Added resume link

--- src/utils/bin/commands.ts
+++ src/utils/bin/commands.ts
@@ -38,156 +38,156 @@ Type 'sumfetch' to display my summary!
 // 'readme' - my github readme.`;
 // };
 
-export const funfacts = async (args: string[]): Promise<string> => {
-  return `List of places fun facts about me:\n
-Kai ig
-6'5 ig
-Nonchalant ig
-Toji build ig
-Radiohead ig
-`
-  ;
-};
-
-export const travel = async (args: string[]): Promise<string> => {
-  return `List of places I want to visit:\n
-US NATIONAL PARKS
----------------
-Crater Lake
-Denali
-Glacier
-Grand Canyon
-Grand Teton
-Great Sand Dunes
-Haleakala
-Mt. Rainier
-Olympic
-Smoky Mountains
-Yellowstone
-Zion
-
-COUNTRIES
----------------
-Belgium
-Chile
-China
-Ghana
-Japan
-Kyrgyzstan
-Morocco
-Nepal
-Switzerland
-Thailand
-  `
-  ;
-};
-
-export const foods = async (args: string[]): Promise<string> => {
-  return `List of my favorite food:\n
-Baked Mac n Cheese
-Biriyani
-Burrito
-Chicken Adobo
-Chicken Alfredo
-Chicken Teriyaki
-Chicken Tikka
-Fried Chicken
-Fried Rice
-Gyro
-Idli
-Jollof Rice (Ghanaian)
-Lasagna
-Paneer Butter Masala
-Payasam
-Pizza
-Quesadilla
-Ramen
-Sambar
-Shawarma
-Sushi
-Tehri
-  `
-  ;
-};
-
-export const movies = async (args: string[]): Promise<string> => {
-  return `List of my favorite movies:\n
-Dune
-Interstellar
-iRobot
-Kung Fu Panda
-Parasite
-Perks of Being A Wallflower
-Puss In Boots: The Last Wish
-Ratatouille
-Silent Voice
-The Batman
-The Emporer's New Groove
-Uncut Gems
-Up
-Wall-E
-Whiplash
-  `
-  ;
-};
-
-export const games = async (args: string[]): Promise<string> => {
-  return `List of my favorite games:\n
-Batman: Arkham Asylum
-Batman: Arkham Knight
-DOOM: Eternal
-Dark Souls III
-Elden Ring
-Geometry Dash
-Ghost of Tsushima
-God of War
-Hades
-Halo 4
-Hollow Knight
-Injustice: Gods Among Us
-Overwatch
-Red Dead Redemption 2
-Sekiro: Shadows Die Twice
-Stray
-Titanfall 2
-Ultrakill
-`
-  ;
-};
-
-export const music = async (args: string[]): Promise<string> => {
-  return `List of my favorite music artists:\n
-21 Savage
-Billie Eilish
-Boa
-Crying City
-Deftones
-Djo
-Green Day
-Jhene Aiko
-Kali Uchis
-Kendrick Lamar
-Laufey
-Linkin Park
-Los Retros
-MF Doom
-Mac DeMarco
-Nirvana
-Panchiko
-Paramore
-Radiohead
-System Of A Down
-Tame Impala
-The Marias
-Tyler, The Creator
-`
-  ;
-};
-
-// export const resume = async (args: string[]): Promise<string> => {
-//   window.open(`${config.resume_url}`);
-//   return 'Opening resume...';
+// export const funfacts = async (args: string[]): Promise<string> => {
+//   return `List of places fun facts about me:\n
+// Kai ig
+// 6'5 ig
+// Nonchalant ig
+// Toji build ig
+// Radiohead ig
+// `
+//   ;
 // };
+
+// export const travel = async (args: string[]): Promise<string> => {
+//   return `List of places I want to visit:\n
+// US NATIONAL PARKS
+// ---------------
+// Crater Lake
+// Denali
+// Glacier
+// Grand Canyon
+// Grand Teton
+// Great Sand Dunes
+// Haleakala
+// Mt. Rainier
+// Olympic
+// Smoky Mountains
+// Yellowstone
+// Zion
+
+// COUNTRIES
+// ---------------
+// Belgium
+// Chile
+// China
+// Ghana
+// Japan
+// Kyrgyzstan
+// Morocco
+// Nepal
+// Switzerland
+// Thailand
+//   `
+//   ;
+// };
+
+// export const foods = async (args: string[]): Promise<string> => {
+//   return `List of my favorite food:\n
+// Baked Mac n Cheese
+// Biriyani
+// Burrito
+// Chicken Adobo
+// Chicken Alfredo
+// Chicken Teriyaki
+// Chicken Tikka
+// Fried Chicken
+// Fried Rice
+// Gyro
+// Idli
+// Jollof Rice (Ghanaian)
+// Lasagna
+// Paneer Butter Masala
+// Payasam
+// Pizza
+// Quesadilla
+// Ramen
+// Sambar
+// Shawarma
+// Sushi
+// Tehri
+//   `
+//   ;
+// };
+
+// export const movies = async (args: string[]): Promise<string> => {
+//   return `List of my favorite movies:\n
+// Dune
+// Interstellar
+// iRobot
+// Kung Fu Panda
+// Parasite
+// Perks of Being A Wallflower
+// Puss In Boots: The Last Wish
+// Ratatouille
+// Silent Voice
+// The Batman
+// The Emporer's New Groove
+// Uncut Gems
+// Up
+// Wall-E
+// Whiplash
+//   `
+//   ;
+// };
+
+// export const games = async (args: string[]): Promise<string> => {
+//   return `List of my favorite games:\n
+// Batman: Arkham Asylum
+// Batman: Arkham Knight
+// DOOM: Eternal
+// Dark Souls III
+// Elden Ring
+// Geometry Dash
+// Ghost of Tsushima
+// God of War
+// Hades
+// Halo 4
+// Hollow Knight
+// Injustice: Gods Among Us
+// Overwatch
+// Red Dead Redemption 2
+// Sekiro: Shadows Die Twice
+// Stray
+// Titanfall 2
+// Ultrakill
+// `
+//   ;
+// };
+
+// export const music = async (args: string[]): Promise<string> => {
+//   return `List of my favorite music artists:\n
+// 21 Savage
+// Billie Eilish
+// Boa
+// Crying City
+// Deftones
+// Djo
+// Green Day
+// Jhene Aiko
+// Kali Uchis
+// Kendrick Lamar
+// Laufey
+// Linkin Park
+// Los Retros
+// MF Doom
+// Mac DeMarco
+// Nirvana
+// Panchiko
+// Paramore
+// Radiohead
+// System Of A Down
+// Tame Impala
+// The Marias
+// Tyler, The Creator
+// `
+//   ;
+// };
+
+export const resume = async (args: string[]): Promise<string> => {
+  window.open(`${config.resume_url}`);
+  return 'Opening resume...';
+};
 
 // // Donate
 // export const donate = async (args: string[]): Promise<string> => {
@@ -199,22 +199,22 @@ Tyler, The Creator
 // };
 
 // Contact
-// export const email = async (args: string[]): Promise<string> => {
-//   window.open(`mailto:${config.email}`);
-//   return `Opening mailto:${config.email}...`;
-// };
+export const email = async (args: string[]): Promise<string> => {
+  window.open(`mailto:${config.email}`);
+  return `Opening mailto:${config.email}...`;
+};
 
-// export const github = async (args: string[]): Promise<string> => {
-//   window.open(`https://github.com/${config.social.github}/`);
+export const github = async (args: string[]): Promise<string> => {
+  window.open(`https://github.com/${config.social.github}/`);
 
-//   return 'Opening github...';
-// };
+  return 'Opening github...';
+};
 
-// export const linkedin = async (args: string[]): Promise<string> => {
-//   window.open(`https://www.linkedin.com/in/${config.social.linkedin}/`);
+export const linkedin = async (args: string[]): Promise<string> => {
+  window.open(`https://www.linkedin.com/in/${config.social.linkedin}/`);
 
-//   return 'Opening linkedin...';
-// };
+  return 'Opening linkedin...';
+};
 
 // Search
 // export const google = async (args: string[]): Promise<string> => {
